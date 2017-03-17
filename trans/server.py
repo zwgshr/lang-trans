470ba15fdfa2bedef9189a7bc1281d21f1bc6b58
张文广 <zwgmlr3@outlook.com>
2017-03-17 22:58:47 +0800

    添加主界面

--- trans/server.py
+++ trans/server.py
@@ -1,12 +1,23 @@
 from flask import request, Flask, render_template, session, redirect, url_for, escape
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = 'temp/'
+ALLOWED_EXTENSIONS = set(['lang', 'py'])
 
 app = Flask(__name__)
 app.secret_key = '123456'
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 上传文件限制
 
 users = {
     'admin@a': 'admin',
     'coc@c': 'admin',
 }
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 def _yz_user(ses):
@@ -23,7 +34,7 @@ def index():
     if not _yz_user(session):
         return redirect(url_for('login'))
     else:
-        return 'Logged in as %s' % escape(session['email'])
+        return render_template('trans.html', name="0")
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -46,6 +57,15 @@ def logout():
     session.pop('email', None)
     session.pop('password', None)
     return redirect(url_for('index'))
+
+
+@app.route('/FileUpload/Upload', methods=['POST'])
+def Upload(**args):
+    file = request.files['file_data']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+    return 'ok'
 
 
 if __name__ == "__main__":
